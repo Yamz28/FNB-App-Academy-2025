dd65a928e72302c9d6289b707381ed79608e22de
Yamz28 <tshiamopadi360@gmail.com>
2025-07-18 10:17:00 +0200

    This is an FNB app academy file i created for my projects

---
+++ Module 11/my_task.py
@@ -0,0 +1,6 @@
+
+def multiply(l, b):
+    return l * b
+
+result = multiply(int(input("Enter the length of the rectangle: ")), int(input("Enter the width of the rectangle: ")))
+print(f"The area of the rectangle is: {result}")
